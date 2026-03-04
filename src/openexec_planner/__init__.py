7100863decaf2f7096c087260eaf4ff05e5fab58
Your Name <you@example.com>
2026-03-04 15:29:49 +0200

    chore: fix CI import errors and improve dependency setup

--- src/openexec_planner/__init__.py
+++ src/openexec_planner/__init__.py
@@ -4,6 +4,7 @@ __version__ = "0.1.0"
 
 from .parser import IntentParser, parse_intent
 from .generator import StoryGenerator, Story
+from .llm_generator import LLMStoryGenerator
 from .goal_tree import GoalTreeBuilder, GoalTree, GoalNode
 from .scheduler import Scheduler, Task
 
@@ -12,6 +13,7 @@ __all__ = [
     "parse_intent",
     "StoryGenerator",
     "Story",
+    "LLMStoryGenerator",
     "GoalTreeBuilder",
     "GoalTree",
     "GoalNode",
